b87a34f9877c4245d21687b2cf7c35a777064e48
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-06-09 22:34:48 -0300

    feat: comandos do teclado para interrupção do SO

--- so-sim-cpp-exercicio/os/os.cpp
+++ so-sim-cpp-exercicio/os/os.cpp
@@ -11,35 +11,67 @@
 #include "os.h"
 #include "os-lib.h"
 
-
-namespace OS {
-
-Arch::Cpu *g_cpu = nullptr;
-std::string command_buffer = "";
-
-// ---------------------------------------
-
-void boot (Arch::Cpu *cpu)
-{
-	terminal_println(cpu, Terminal::Command, "Type commands here");
-	terminal_println(cpu, Terminal::App, "Apps output here");
-	terminal_println(cpu, Terminal::Kernel, "Kernel output here");
-}
-
-// ---------------------------------------
-
-void interrupt (const InterruptCode interrupt)
+namespace OS
 {
 
-}
+	Arch::Cpu *g_cpu = nullptr;
+	std::string command_buffer = "";
 
-// ---------------------------------------
+	// ---------------------------------------
 
-void syscall ()
-{
+	void boot(Arch::Cpu *cpu)
+	{
+		g_cpu = cpu;
+		terminal_println(cpu, Terminal::Command, "Type commands here");
+		terminal_println(cpu, Terminal::App, "Apps output here");
+		terminal_println(cpu, Terminal::Kernel, "Kernel output here");
+	}
 
-}
+	// ---------------------------------------
 
-// ---------------------------------------
+	void interrupt(const InterruptCode interrupt)
+	{
+		if (static_cast<int>(interrupt) == 0)
+		{
+
+			uint16_t txt_char = g_cpu->read_io(2);
+
+			if (txt_char == '\n')
+			{
+
+				if (command_buffer == "fechar" || command_buffer == "exit")
+				{
+					terminal_println(g_cpu, Terminal::Kernel, "Desligando o simulador...");
+					g_cpu->turn_off();
+				}
+				else
+				{
+					terminal_println(g_cpu, Terminal::Kernel, ("Comando invalido: " + command_buffer).c_str());
+				}
+
+				command_buffer = "";
+			}
+			else if (txt_char == 8 || txt_char == 127)
+			{
+				if (!command_buffer.empty())
+				{
+					command_buffer.pop_back();
+				}
+			}
+			
+			else
+			{
+				command_buffer += (char)txt_char;
+			}
+		}
+	}
+
+	// ---------------------------------------
+
+	void syscall()
+	{
+	}
+
+	// ---------------------------------------
 
 } // end namespace OS
